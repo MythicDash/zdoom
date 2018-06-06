b19e2e01913affe56bc3f00e97cee57df18fc935
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2018-06-06 12:46:11 +0100

    Update hakchi_zdoom.sh

--- hakchi/hakchi_zdoom.sh
+++ hakchi/hakchi_zdoom.sh
@@ -25,7 +25,7 @@ if [ "$ok" == 1 ]; then
 
   decodepng "$zDOOMTrueDir/Hakchi_zDOOM_assets/zDoomsplash-min.png" > /dev/fb0;
 
-  #Dynamically link the SDL2.0 library on the mini
+  #Dynamically link the SDL2.0 library on the mini (Doesn't work on FAT32! You will need to rip it from the mini instead)
   [ ! -L $zDOOMTrueDir/lib/libSDL2-2.0.so.0 ] && ln -sf "/usr/lib/libSDL2.so" "$zDOOMTrueDir/lib/libSDL2-2.0.so.0"
 
   #Load in the extra libraries required to run on SNESC
@@ -34,15 +34,17 @@ if [ "$ok" == 1 ]; then
 
   #Change the HOME environment variable for running on the mini...
   mkdir -p /var/lib/hakchi/rootfs/etc/zdoom
+  mkdir -p /var/lib/hakchi/rootfs/etc/zdoom/.config #Pretty sure I can extend the line above but fuck it.
   export HOME="/var/lib/hakchi/rootfs/etc/zdoom"
   
   if [ ! -f /var/lib/hakchi/rootfs/etc/zdoom/.config/zdoom.ini ]; then
     cp $zDOOMTrueDir/zDOOM_files/zdoom.ini /var/lib/hakchi/rootfs/etc/zdoom/.config/
   fi
-
+  
+  chmod +x $zDOOMTrueDir/zDOOM_files/zdoom
   cd $zDOOMTrueDir/zDOOM_files
 
-  $zDOOMTrueDir/zDOOM_files/zdoom -iwad doom.wad -file brutalv20b_hakchi.pk3 +vid_drawfps 1 +snd_output ALSA +snd_listdrivers +snd_listmididevices +snd_status &> /media/zdoom.log #Please god just fucking work.
+  $zDOOMTrueDir/zDOOM_files/zdoom -iwad doom.wad -file brutalv20b_hakchi.pk3 +vid_fps 1 +snd_output ALSA +snd_listdrivers +snd_listmididevices +snd_status &> /media/zdoom.log #Please god just fucking work.
 
   echo 3 > /proc/sys/vm/drop_caches #Clear down after ourselves...
 
